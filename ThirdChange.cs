09dc4b8be5f722638ba88dea7755a7a3637aa4b4
juribe21 <32712610+juribe21@users.noreply.github.com>
2022-04-12 09:02:29 -0700

    Create ThirdChange.cs
    Add ThirdChange.cs

---
+++ ThirdChange.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace com.ams.pistola.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThirdChangeController : ControllerBase
+    {
+    }
+}
