082050abc0cf2d58ed6dddd5993881ee3b3aad8b
juribe21 <32712610+juribe21@users.noreply.github.com>
2022-04-12 08:31:34 -0700

    Update FirstChange.cs
    Add login HJ

--- FirstChange.cs
+++ FirstChange.cs
@@ -14,5 +14,27 @@ namespace com.ams.pistola.api.Controllers
         {
         }
         
+        [HttpPost]
+        [Route("api/login")]
+        public IActionResult loginHJ([FromBody] LoginDTO user) {
+            IActionResult response = Unauthorized();
+            JWT jwt = new JWT(_config);
+            var usr = _context.Employees.Where(w => w.Id == user.userName && w.Password == user.password && w.Enabled == true).FirstOrDefault();
+            if (usr != null) {
+                var roles = _context.UserRole.Where(w => w.UserId == usr.EmployeeId).Select(s => s.RoleId).ToList();
+                var permissions  = _context.RolePermission.Where(w => roles.Contains(w.RoleId)).Select(s => s.Permission.Permission1).ToList();
+
+                usr.LastLoginDate = DateTime.Now;
+                _context.SaveChanges();
+                    var tokenString = jwt.GenerateJWTToken(usr, permissions);
+                response = Ok(new
+                {
+                    token = tokenString,
+                    userDetails = usr,
+                });
+            }
+            return response;
+        }
+        
     }
 }
