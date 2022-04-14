20de0bf80b2b533c772336154954c6be8ecb1c41
AzureAD\JorgeUribeP <jorge.u@adoremeservices.com>
2022-04-14 07:35:44 -0700

    Add using block

--- April 14 2022 A.cs
+++ April 14 2022 A.cs
@@ -1,3 +1,17 @@
+using com.ams.pistola.models.DTO;
+using com.ams.pistola.data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using com.ams.pistola.models;
+using com.ams.pistola.api.Classes;
+using EFCore.BulkExtensions;
+
+namespace com.ams.pistola.api.Controllers {
+
  [HttpPost]
         [Route("api/returns/item")]
         public AmTReturnedItem returnItem([FromBody] ReturnedItemDTO item) 
@@ -18,3 +32,4 @@
             _context.SaveChanges();
             return _context.AmTReturnedItem.Where(w => w.Id == ri.Id).FirstOrDefault();
         }
+}
